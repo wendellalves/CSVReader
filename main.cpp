c747d184d5ccb43fc28c36158d006d590dcbc466
wendellalves <wendell.a.silva2@gmail.com>
2018-05-21 12:38:55 -0300

    Funcionando para o CSV Gerado pela SOM

--- main.cpp
+++ main.cpp
@@ -4,11 +4,12 @@
 
 int main()
 {   
-        std::string x,y,z,aux;
-        int cont = 0;
+        std::string x, y, z, iPos, jPos;
+        int sizeNetwork = 10;
+        //int cont = 0;
         
 	// Creating an object of CSVWriter
-	CSVReader reader("teste.txt");
+	CSVReader reader("texte2.txt");
         
         //std::ofstream myfile;
         //myfile.open ("saida.csv");
@@ -16,32 +17,48 @@ int main()
 	// Get the data from CSV File
 	std::vector<std::vector<std::string> > dataList = reader.getData();
         
-        //Testando o acesso a um unico dado do vetor
+        
+        for(int i = 1; i <= sizeNetwork*sizeNetwork; i++){
+                iPos = dataList[i][0];
+                jPos = dataList[i][0+1];
+                x = dataList[i][0+2];
+                y = dataList[i][0+3];
+                z = dataList[i][0+4];
+                std::cout << "i = " << iPos << std::endl;
+                std::cout << "j = " << jPos << std::endl;
+                std::cout << "x = " << x << std::endl;
+                std::cout << "y = " << y << std::endl;
+                std::cout << "z = " << z << std::endl;
+                std::cout << std::endl;
+        }
+            
+        /*//Testando o acesso a um unico dado do vetor
         for(std::vector<std::string> vec : dataList)
 	{
 		for(std::string data : vec)
 		{                   
-                    if(cont > 0){
-                        x = data.substr(0, data.find(" "));
-                        aux = data.substr(data.find(" ")+1, data.length());
-                        y = aux.substr(0, aux.find(" "));
-                        z = aux.substr(aux.find(" ")+1, aux.length());
+                    if(cont > 4){
+                        i = data;
+                        //aux = data.substr(data.find(",")+1, data.length());
+                        //y = aux.substr(0, aux.find(" "));
+                        //z = aux.substr(aux.find(" ")+1, aux.length());
                     
-                        std::cout << "x = " << x << std::endl;
+                        //std::cout << data << std::endl;
+                        //std::cout << "i = " << i << std::endl;
                         //std::cout << "aux =" << aux << std::endl;
-                        std::cout << "y = " << y << std::endl;
-                        std::cout << "z = " << z << std::endl;
+                        //std::cout << "y = " << y << std::endl;
+                        //std::cout << "z = " << z << std::endl;
                         
                         //std::cout << cont << std::endl;
                         std::cout<<std::endl;
                     }
-                    cont++; 
+                    //cont++; 
 		}
 	}
         
-        cont = 0;
+        //cont = 0;
  
-	/*// Print the content of row by row on screen
+	// Print the content of row by row on screen
 	for(std::vector<std::string> vec : dataList)
 	{
 		for(std::string data : vec)
